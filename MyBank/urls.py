2f93c6c811aa162f7eed6bcc20e56f6e414a64e6
Yaswanth <yaswanthalapati20@gmail.com>
2021-07-09 10:09:51 -0700

    urls changed

--- MyBank/urls.py
+++ MyBank/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('Customers/',include('Customers.urls')),
     path('admin/', admin.site.urls),
+    path('',include('Customers.urls')),
 ]
