7313e3e8361566a6819aadce2410a995f45b5fbc
Yaswanth <yaswanthalapati20@gmail.com>
2021-07-09 10:22:32 -0700

    urls and static dirs

--- MyBank/urls.py
+++ MyBank/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Customers.urls')),
+    path('Customer/',include('Customers.urls')),
 ]
